94a66701d70aaf7d06986170304d26cc7424d9b8
Leandro Paganotti <38894043+leandropaganotti@users.noreply.github.com>
2018-06-16 19:12:39 -0400

    - made # of threads == image height

--- src/main.cpp
+++ src/main.cpp
@@ -9,7 +9,6 @@
 using namespace std;
 
 const char *xmlscene = NULL;// xml file with scene description
-unsigned    nthreads = 4;   // number of threads to run
 unsigned    nrays    = 1;   // nrays * nrays rays will be cast for each pixel
 unsigned    nimages  = 1;   // generates nimages from difterent angles around y-axis
 
@@ -35,13 +34,19 @@ int main(int argc, char **argv)
 
     const Vector3f from( scene.cameraOptions.getFrom() ), to( scene.cameraOptions.getTo());
 
+    cout << "Render parameters:" << endl;
+    cout << "- scene file: " << xmlscene << endl;
+    cout << "- # of image around y-axis: " << nimages << endl;
+    cout << "- image output resolution: " << camera.getWidth() << "x" << camera.getHeight() << endl;
+    cout << "- # of threads: " << camera.getHeight() << endl;
+
     for (unsigned i=0; i < nimages; ++i)
     {
         cout << "\r" << i+1 << "/" << nimages << ": at " << std::fixed  << std::setw(6) <<  std::setprecision( 2 ) <<  i*angle << "° => ";
         camera.lookAt( T(to) * Ry(deg2rad( i*angle )) * T(-to) * from, to); // rotate around y-axis
 
         auto start = chrono::steady_clock::now();       
-        render.render(scene, nrays, nthreads);
+        render.render(scene, nrays, camera.getHeight());
         auto end = chrono::steady_clock::now();
 
         elapsedTime = chrono::duration_cast<chrono::milliseconds>(end - start).count();
@@ -61,19 +66,16 @@ int main(int argc, char **argv)
 
 void usage (char **argv)
 {
-    fprintf(stderr, "Usage: %s [-t nthreads] [-r nrays] [-i nimages] xmlfile\n\n", argv[0]);
+    fprintf(stderr, "Usage: %s [-r nrays] [-i nimages] xmlfile\n\n", argv[0]);
 }
 
 void parseArguments(int argc, char **argv)
 {
     int opt;
-    while ((opt = getopt(argc, argv, "r:t:i:")) != -1) {
+    while ((opt = getopt(argc, argv, "r:i:")) != -1) {
         switch (opt) {
         case 'r':
             nrays = atoi(optarg);
-            break;
-        case 't':
-            nthreads = atoi(optarg);
             break;
         case 'i':
             nimages = atoi(optarg);
@@ -89,10 +91,4 @@ void parseArguments(int argc, char **argv)
         exit(EXIT_FAILURE);
     }
    xmlscene = argv[optind];
-
-   cout << "Argument list: " << endl;
-   cout << "xmlfile: " << xmlscene <<  endl;
-   cout << "nimages: " << nimages << endl;
-   cout << "nrays: " << nrays << endl;
-   cout << "nthreads: " << nthreads << endl << endl;
 }
