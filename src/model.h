4e5c1b2a296f531b8c17f3171f06099d2f9f07ca
Leandro Paganotti <lpaganotti@magicleap.com>
2018-10-05 08:34:07 -0400

    - monkey model scene adjust

--- src/model.h
+++ src/model.h
@@ -3,6 +3,14 @@
 #include <string>
 #include "mesh.h"
 
+/**************************************************************
+TODO: A model is going to be a collections of Mesh blocks,
+which one with their material, transformation, and texture
+
+current implementation loads a mesh from .obj file
+
+**************************************************************/
+
 class Model: public Mesh
 {
 public:
