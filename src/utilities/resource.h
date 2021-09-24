dfe340d142857ccc13e6260b6b204bce9fc675f7
Leandro Paganotti <leandropaganotti@gmail.com>
2021-09-24 11:07:33 -0400

    - Lazy initialziation solution in Resource class

--- src/utilities/resource.h
+++ src/utilities/resource.h
@@ -4,41 +4,59 @@
 #include <memory>
 #include <map>
 #include "paramset.h"
+#include <iostream>
+
+template<class T>
+using ResourceMap = std::unordered_map<std::string, std::shared_ptr<T>>;
 
 template<class T>
 class Resource
 {
 public:
-    static std::shared_ptr<T> Create(const std::string &key, const ParamSet &params)
-    {
-        std::shared_ptr<T> res = T::Create(params);
-        if(res)
-            resources[key] = res;
-        return res;
-    }
     static std::shared_ptr<T> Create(const std::string &key)
     {
         std::shared_ptr<T> res = T::Create();
         if (res)
-            resources[key] = res;
+            GetResources()[key] = res;
         return res;
     }
+
+    template <typename ... Args>
+    static std::shared_ptr<T> Create(const std::string &key, Args&& ... args)
+    {
+        std::shared_ptr<T> res = T::Create(args...);
+        GetResources()[key] = res;
+        return res;
+    }
+
+    static std::shared_ptr<T> Create(const std::string &key, const ParamSet &params)
+    {
+        std::shared_ptr<T> res = T::Create(params);
+        if(res)
+            GetResources()[key] = res;
+        return res;
+    }
+
     static std::shared_ptr<T> Get(const std::string &key)
     {
-        if(resources.find(key) == resources.end())
+        if(GetResources().find(key) == GetResources().end())
             return nullptr;
 
-        return resources[key];
+        return GetResources()[key];
     }
+
     static void Add(const std::string &key, std::shared_ptr<T> &res)
     {
-        resources[key] = res;
+        if(res)
+            GetResources()[key] = res;
     }
 
 protected:
     Resource() = delete;
-    static std::map<std::string, std::shared_ptr<T>> resources;
-};
 
-template<class T>
-std::map<std::string, std::shared_ptr<T>> Resource<T>::resources;
+    static ResourceMap<T>& GetResources()
+    {
+        static ResourceMap<T> *ans = new ResourceMap<T>();
+        return *ans;
+    }
+};
